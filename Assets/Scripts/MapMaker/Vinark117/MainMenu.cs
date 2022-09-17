5705959d27404fd7e6acef3fc2bf57bf624b4402
RussiSunni <jonathandyason@hotmail.com>
2022-09-17 14:49:51 +0200

    MainMenu

--- Assets/Scripts/MapMaker/Vinark117/MainMenu.cs
+++ Assets/Scripts/MapMaker/Vinark117/MainMenu.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void LoadMapMaker()
     {
+        SceneManager.LoadScene("MapMaker");
+    }
 
+    public void LoadPlayer()
+    {
+        SceneManager.LoadScene("Player");
     }
 }
