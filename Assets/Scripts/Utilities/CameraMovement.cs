316a98f32a08d5a62dcc1bc638273d2fbdf8582c
RussiSunni <jonathandyason@hotmail.com>
2022-10-21 07:59:53 +0200

    More options for panning.

--- Assets/Scripts/Utilities/CameraMovement.cs
+++ Assets/Scripts/Utilities/CameraMovement.cs
@@ -9,21 +9,40 @@ public class CameraMovement : MonoBehaviour
 
     private Vector3 dragOrigin;
 
+    public int speed = 5;
+
     private void Update()
     {
+        // Pan with middles mouse button, if available.
         PanCamera();
 
-      //  Debug.Log(Input.GetTouch(0).position);
+        //  ALternatively, with arrow keys, or a, w, s, d.
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            cam.transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            cam.transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            cam.transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            cam.transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
     }
 
     private void PanCamera()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(2))
         {
             dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
         }
        
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(2))
         {
             Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
 
